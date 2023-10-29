9828bc8982758c6600bf33c1c94e203d140b9736
gurielamp <gurielamp@gmail.com>
2023-10-30 06:06:16 +0700

    remove typo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,6 @@ export default async function Home() {
   const articleData = await getArticle()
   return (
     <>
-      <h1>Docs</h1>
       <SearchArticle articleData={articleData} />
     </>
   )
